636c91d43898e938e5894c692aecd21a3aaffc7d
Noah Loomans <noahloomans@gmail.com>
2018-08-20 17:51:17 +0200

    Add comment

--- src/main/FlashPlayer.ts
+++ src/main/FlashPlayer.ts
@@ -10,6 +10,9 @@ export default class FlashPlayer {
     const root: string = this.flashpointPath + '/Arcade';
     const filename: string = path.resolve(root, applicationPath);
 
+    // When using Linux, use the proxy created in BackgroundServices.ts
+    // This is only needed on Linux because the proxy is installed on system
+    // level entire system when using Windows.
     const env = process.platform === 'linux'
       ? { ...process.env, http_proxy: 'http://localhost:22500/' }
       : process.env;
