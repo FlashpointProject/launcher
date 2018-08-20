7daf64b82cbd3c13cbc9df5fdee40a62ad120bb9
Noah Loomans <noahloomans@gmail.com>
2018-08-20 21:58:52 +0200

    Fix indentation

--- src/main/BackgroundServices.ts
+++ src/main/BackgroundServices.ts
@@ -2,43 +2,43 @@ import * as path from 'path';
 import ManagedChildProcess from './ManagedChildProcess';
 
 export default class BackgroundServices {
-	private router?: ManagedChildProcess;
+  private router?: ManagedChildProcess;
 
-	constructor(
-		private flashpointPath: string,
-	) {};
+  constructor(
+    private flashpointPath: string,
+  ) {};
 
-	/**
-	 * Start all required background process for this platform
-	 */
-	start() {
-		switch (process.platform) {
-			case 'linux':
-				return this.startLinux();
-			default:
-				// @TODO: Add Windows support
-		}
-	}
+  /**
+   * Start all required background process for this platform
+   */
+  start() {
+    switch (process.platform) {
+      case 'linux':
+        return this.startLinux();
+      default:
+        // @TODO: Add Windows support
+    }
+  }
 
-	private startLinux() {
-		const serverPath = path.join(this.flashpointPath, './Arcade/Games/Flash');
+  private startLinux() {
+    const serverPath = path.join(this.flashpointPath, './Arcade/Games/Flash');
 
-		// @TODO: Figure out if this is the same for the regular Flashpoint
-		// This has only been tested with Flashpoint Infinity
-		this.router = new ManagedChildProcess(
-			'Redirector',
-			'php',
-			['-S', 'localhost:22500', 'router.php'],
-			serverPath,
-		);
-	}
+    // @TODO: Figure out if this is the same for the regular Flashpoint
+    // This has only been tested with Flashpoint Infinity
+    this.router = new ManagedChildProcess(
+      'Redirector',
+      'php',
+      ['-S', 'localhost:22500', 'router.php'],
+      serverPath,
+    );
+  }
 
-	/**
-	 * Stop all currently active background processes
-	 */
-	stop() {
-		if (this.router) {
-			this.router.kill();
-		}
-	}
+  /**
+   * Stop all currently active background processes
+   */
+  stop() {
+    if (this.router) {
+      this.router.kill();
+    }
+  }
 }
