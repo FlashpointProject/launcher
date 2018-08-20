9e8517b2ae3739aa794f97db475127ac50ceaca9
Noah Loomans <noahloomans@gmail.com>
2018-08-20 20:37:15 +0200

    Use the more common name "Redirector" instead of "router"

--- src/main/BackgroundServices.ts
+++ src/main/BackgroundServices.ts
@@ -11,8 +11,10 @@ export default class BackgroundServices {
 	start() {
 		const serverPath = path.join(this.flashpointPath, './Arcade/Games/Flash');
 
+		// @TODO: Figure out if this is the same for the regular Flashpoint
+		// This has only been tested with Flashpoint Infinity
 		this.router = new ManagedChildProcess(
-			'router',
+			'Redirector',
 			'php',
 			['-S', 'localhost:22500', 'router.php'],
 			serverPath,
