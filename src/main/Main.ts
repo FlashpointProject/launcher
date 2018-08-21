8ff37a6686b0b00a04f7c4f2589b5a15c5ba5230
Noah Loomans <noahloomans@gmail.com>
2018-08-21 21:02:59 +0200

    Add permission rejection back

--- src/main/Main.ts
+++ src/main/Main.ts
@@ -1,4 +1,4 @@
-import { app, ipcMain, session, WebContents } from 'electron';
+import { app, session, ipcMain } from 'electron';
 import MainWindow from './MainWindow';
 import * as Util from './Util';
 import { AppConfig } from '../shared/config/AppConfig';
@@ -36,23 +36,25 @@ export class Main {
   }
 
   private onAppReady() {
+    if (session.defaultSession) {
+      // Reject all permission requests since we don't need any permissions.
+      session.defaultSession.setPermissionRequestHandler(
+        (webContents, permission, callback) => callback(false)
+      );
+
+      // Stop non-local resources from being fetched (as long as their response has at least one header?)
+      // Only allow local scripts to execute (Not sure what this allows? "file://"? "localhost"?)
+      // (TypeScript type information is missing, check the link below for the type info)
+      // https://github.com/electron/electron/blob/master/docs/api/web-request.md#webrequestonheadersreceivedfilter-listener
+      session.defaultSession.webRequest.onHeadersReceived(
+        (details: any, callback: Function) => callback({
+          responseHeaders: `script-src 'self'`,
+          cancel: true
+        })
+      );
+    }
+
     this._mainWindow.createWindow();
-    // Stop non-local resources from being fetched (as long as their response has at least one header?)
-    // Only allow local scripts to execute (Not sure what this allows? "file://"? "localhost"?)
-    // (TypeScript type information is missing, check the link below for the type info)
-    // https://github.com/electron/electron/blob/master/docs/api/web-request.md#webrequestonheadersreceivedfilter-listener
-    (session.defaultSession as Electron.Session).webRequest.onHeadersReceived((details: any, callback: Function) => {
-      callback({
-        responseHeaders: `script-src 'self'`,
-        cancel: true
-      });
-    });
-    // (Called when the renderer requests access to things like webcam, microphone etc.)
-    (session.defaultSession as Electron.Session).setPermissionRequestHandler(
-      function onPermissionRequest(webContents, permission, callback, details) {
-        return callback(false); // Deny all requests
-      }
-    );
   }
 
   private onAppWindowAllClosed() {
