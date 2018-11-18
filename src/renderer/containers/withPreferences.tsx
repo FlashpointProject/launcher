4973932d798c302c2def312a22301f3178e7ddb0
bubba <darkclow3r@gmail.com>
2018-11-18 23:46:52 +0100

    fix variable name

--- src/renderer/containers/withPreferences.tsx
+++ src/renderer/containers/withPreferences.tsx
@@ -2,7 +2,7 @@ import { Dispatch, bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { ApplicationState } from '../store';
 import { IAppPreferencesData } from '../../shared/preferences/IAppPreferencesData';
-import * as searchActions from '../store/preferences/actions';
+import * as action from '../store/preferences/actions';
 
 interface IStateToProps {
   readonly preferencesData: Readonly<IAppPreferencesData>;
@@ -19,7 +19,7 @@ const mapStateToProps = ({ preferences }: ApplicationState): IStateToProps => ({
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
-  updatePreferences: (data: Partial<IAppPreferencesData>) => searchActions.updatePreferences(data)
+  updatePreferences: (data: Partial<IAppPreferencesData>) => action.updatePreferences(data)
 }, dispatch);
 
 export const withPreferences = connect(
