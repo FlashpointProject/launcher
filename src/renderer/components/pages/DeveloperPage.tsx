5b5196ea3476b3da75d321c492fa536022fb3f7c
bubba <darkclow3r@gmail.com>
2018-12-30 00:14:10 +0100

    feat: add "Check Game Titles" tool to the developer page

--- src/renderer/components/pages/DeveloperPage.tsx
+++ src/renderer/components/pages/DeveloperPage.tsx
@@ -4,7 +4,7 @@ import { SimpleButton } from '../SimpleButton';
 import { ICentralState } from '../../interfaces';
 import { IGameInfo } from '../../../shared/game/interfaces';
 import { GameImageCollection } from '../../image/GameImageCollection';
-import * as validateUUID from 'uuid-validate';
+import { validateSemiUUID } from '../../uuid';
 
 interface IDeveloperPageProps {
   central: ICentralState;
@@ -18,7 +18,7 @@ export class DeveloperPage extends React.Component<IDeveloperPageProps, IDevelop
   constructor(props: IDeveloperPageProps) {
     super(props);
     this.state = {
-      text: 'SUPER\nMARIO\nSUPER\nSHOW',
+      text: '',
     };
   }
 
@@ -29,10 +29,14 @@ export class DeveloperPage extends React.Component<IDeveloperPageProps, IDevelop
         <div className='developer-page__inner'>
           <h1 className='developer-page__title'>Developer</h1>
           This is where all the useful developer tools will go.
-          <div>
-            <SimpleButton value='Check Missing Images' onClick={this.onCheckMissingImagesClick} />
-            <SimpleButton value='Check Game IDs' onClick={this.onCheckGameIDsClick} />
-            <LogData className='developer-page__content' logData={text} />
+          <div className='developer-page__buttons'>
+            <SimpleButton value='Check Missing Images' onClick={this.onCheckMissingImagesClick}
+                          title='List all games without a thumbnail or screenshot.' />
+            <SimpleButton value='Check Game IDs' onClick={this.onCheckGameIDsClick}
+                          title='List all games with duplicate or invalid IDs' />
+            <SimpleButton value='Check Game Titles' onClick={this.onCheckGameNamesClick}
+                          title='List all games with duplicate titles' />
+            <LogData className='developer-page__log' logData={text} />
           </div>
         </div>
       </div>
@@ -48,6 +52,11 @@ export class DeveloperPage extends React.Component<IDeveloperPageProps, IDevelop
   private onCheckGameIDsClick = (): void => {
     const games = this.props.central.games.collection.games;
     this.setState({ text: checkGameIDs(games) });
+  }
+
+  private onCheckGameNamesClick = (): void => {
+    const games = this.props.central.games.collection.games;
+    this.setState({ text: checkGameTitles(games) });
   }
 }
 
@@ -68,7 +77,7 @@ function checkMissingGameImages(games: IGameInfo[], gameImages: GameImageCollect
   const timeEnd = Date.now(); // End timing
   // Write log message
   let text = '';
-  text += `Check for games with missing images (in ${timeEnd - timeStart}ms)\n`;
+  text += `Checked games for missing images (in ${timeEnd - timeStart}ms)\n`;
   text += `\n`;
   text += `Games with missing thumbnails (${missingThumbnails.length}):\n`;
   missingThumbnails.forEach((game) => { text += `"${game.title}" (ID: ${game.id})\n`; });
@@ -87,25 +96,49 @@ function checkGameIDs(games: IGameInfo[]): string {
   for (let i = 0; i < games.length - 1; i++) {
     const game = games[i];
     // Check if ID is valid
-    if (!validateUUID(game.id, 4)) {
+    if (!validateSemiUUID(game.id)) {
       invalidIDs.push(game);
     }
     // Add ID to registry (to check for duplicates)
     if (!ids[game.id]) { ids[game.id] = []; }
-    else { dupes.push(game.id); }
+    else if (ids[game.id].length === 1) { dupes.push(game.id); }
     ids[game.id].push(game);
   }
   const timeEnd = Date.now(); // End timing
   // Write log message
   let text = '';
-  text += `Check for games with duplicate IDs (in ${timeEnd - timeStart}ms)\n`;
+  text += `Checked games for duplicate and invalid IDs (in ${timeEnd - timeStart}ms)\n`;
   text += `\n`;
   text += `Games with duplicate IDs:\n`;
   dupes.forEach(id => {
-    text += `ID: "${id}" | Games: ${ids[id].map(game => `"${game.title}"`).join(', ')}\n`;
+    text += `ID: "${id}" | Games (${ids[id].length}): ${ids[id].map(game => `"${game.title}"`).join(', ')}\n`;
   });
   text += `\n`;
   text += `Games with invalid IDs (${invalidIDs.length}):\n`;
   invalidIDs.forEach(game => { text += `"${game.title}" (ID: ${game.id})\n`; });
+  return text;
+}
+
+function checkGameTitles(games: IGameInfo[]): string {
+  const timeStart = Date.now(); // Start timing
+  // Find all games with duplicate titles
+  const titles: { [key: string]: IGameInfo[] } = {};
+  const dupes: string[] = [];
+  for (let i = 0; i < games.length - 1; i++) {
+    const game = games[i];
+    // Add name to registry (to check for duplicates)
+    if (!titles[game.title]) { titles[game.title] = []; }
+    else if (titles[game.title].length === 1) { dupes.push(game.title); }
+    titles[game.title].push(game);
+  }
+  const timeEnd = Date.now(); // End timing
+  // Write log message
+  let text = '';
+  text += `Checked all games for duplicate titles (in ${timeEnd - timeStart}ms)\n`;
+  text += `\n`;
+  text += `Games with duplicate titles:\n`;
+  dupes.forEach(name => {
+    text += `Name: "${name}" | Games (${titles[name].length}): ${titles[name].map(game => `${game.id}`).join(', ')}\n`;
+  });
   return text;
 }
--- src/renderer/uuid.ts
+++ src/renderer/uuid.ts
@@ -37,3 +37,48 @@ function getRandomBytes(count: number): Buffer {
 export function uuid() {
   return guid({ random: getRandomBytes(16) });
 }
+
+/**
+ * Check if a semi-UUIDv4 string is valid
+ * (since a lot of games in Flashpoint does not follow the UUIDv4 spec entierly
+ *  we have to make a more lenient script for validating them)
+ * (A "semi-UUIDv4" follows the following pattern:
+ *  "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" where "x" is any hexadecimal character)
+ * @param uuid Semi-UUIDv4 string to check the validity of
+ * @returns Whether or not the argument is a valid semi-UUIDv4 string
+ */
+export function validateSemiUUID(uuid: string): boolean {
+  if (uuid.length !== 36) { return false; }
+  for (let i = 0; i < 36; i++) {
+    switch (i) {
+      case 8:
+      case 13:
+      case 18:
+      case 23:
+        if (uuid[i] !== '-') { return false; }
+        break;
+      default:
+        switch (uuid[i]) {
+          case '0':
+          case '1':
+          case '2':
+          case '3':
+          case '4':
+          case '5':
+          case '6':
+          case '7':
+          case '8':
+          case '9':
+          case 'a':
+          case 'b':
+          case 'c':
+          case 'd':
+          case 'e':
+          case 'f': break;
+          default: return false;
+        }
+        break;
+    }
+  }
+  return true;
+}
