38acd0ec48daab91902cb564de4c7377fd884d77
bubba <darkclow3r@gmail.com>
2019-02-18 19:43:41 +0100

    feat: add "simple columns" css classes and re-organize about page
    Add a two new css classes used to organize elements into columns.
    Use these css classes (with some other css) to organize the about page
    into two columns that are displayed side by side (horizontally) as long
    as there is enough room. If there isn't enough room they will be
    displayed vertically (the left column will be placed on top of the right
    column).

--- src/renderer/components/pages/AboutPage.tsx
+++ src/renderer/components/pages/AboutPage.tsx
@@ -28,44 +28,52 @@ export class AboutPage extends React.Component<IAboutPageProps, IAboutPageState>
         <div className='about-page__inner'>
           <div className="about-page__top">
             <h1 className='about-page__title'>About</h1>
-            {/* Credits */}
             <CreditsTooltip profile={profile} />
-            <div className='about-page__credits'>
-              <div className='about-page__credits__title'>Credits</div>
-              <div className='about-page__credits__profiles'>
-                { (creditsDoneLoading && creditsData) ? (
-                  creditsData.profiles.map((profile, index) => (
-                    <CreditsIcon key={index} profile={profile}
-                                 onMouseEnter={this.onMouseEnterCreditsIcon}
-                                 onMouseLeave={this.onMouseLeaveCreditsIcon}/>
-                  ))
-                ) : ('...') }
-              </div>
-            </div>
-            {/* About Flashpoint Launcher */}
-            <div className='about-page__section'>
-              <p className='about-page__section__title'>Flashpoint Launcher</p>
-              <div className='about-page__section__content'>
-                <p className="about-page__section__content__description">
-                  An open-source desktop application used to browse, manage and play games from the Flashpoint project.
-                </p>
-                <p><b>Version:</b> {appVersionString}</p>
-                <p><b>License:</b> MIT (Read the file named "LICENSE" for more information)</p>
-                <div className='about-page__section__links'>
-                  {link('Github', 'https://github.com/FlashpointProject/launcher')}
+            <div className='about-page__columns simple-columns'>
+              {/* Left Column */}
+              <div className='about-page__columns__left simple-columns__column'>
+                {/* About Flashpoint */}
+                <div className='about-page__section'>
+                  <p className='about-page__section__title'>BlueMaxima's Flashpoint</p>
+                  <div className='about-page__section__content'>
+                    <p className="about-page__section__content__description">
+                      The preservation project this launcher was built for, that aims to be an archive, museum and playable collection of web-based Internet games.
+                    </p>
+                    <div className='about-page__section__links'>
+                      {link('Website', 'http://bluemaxima.org/flashpoint/')}
+                      {link('Discord', 'https://discord.gg/Nc3DScn')}
+                    </div>
+                  </div>
+                </div>
+                {/* About Flashpoint Launcher */}
+                <div className='about-page__section'>
+                  <p className='about-page__section__title'>Flashpoint Launcher</p>
+                  <div className='about-page__section__content'>
+                    <p className="about-page__section__content__description">
+                      An open-source desktop application used to browse, manage and play games from the Flashpoint project.
+                    </p>
+                    <p><b>Version:</b> {appVersionString}</p>
+                    <p><b>License:</b> MIT (Read the file named "LICENSE" for more information)</p>
+                    <div className='about-page__section__links'>
+                      {link('Github', 'https://github.com/FlashpointProject/launcher')}
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            {/* About Flashpoint */}
-            <div className='about-page__section'>
-              <p className='about-page__section__title'>BlueMaxima's Flashpoint</p>
-              <div className='about-page__section__content'>
-                <p className="about-page__section__content__description">
-                  The preservation project this launcher was built for, that aims to be an archive, museum and playable collection of web-based Internet games.
-                </p>
-                <div className='about-page__section__links'>
-                  {link('Website', 'http://bluemaxima.org/flashpoint/')}
-                  {link('Discord', 'https://discord.gg/Nc3DScn')}
+              {/* Right Column */}
+              <div className='about-page__columns__right simple-columns__column'>
+                {/* Credits */}
+                <div className='about-page__credits'>
+                  <div className='about-page__credits__title'>Credits</div>
+                  <div className='about-page__credits__profiles'>
+                    { (creditsDoneLoading && creditsData) ? (
+                      creditsData.profiles.map((profile, index) => (
+                        <CreditsIcon key={index} profile={profile}
+                                    onMouseEnter={this.onMouseEnterCreditsIcon}
+                                    onMouseLeave={this.onMouseLeaveCreditsIcon}/>
+                      ))
+                    ) : ('...') }
+                  </div>
                 </div>
               </div>
             </div>
