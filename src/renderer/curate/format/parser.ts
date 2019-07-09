ec99a0526fe42660fbf1ecebdea9fb94f3556f0b
bubba <darkclow3r@gmail.com>
2019-07-09 20:43:23 +0200

    feat: add a parser for the new curation format
    A parser has been added for the new curation format.
    It is divided into two steps: tokenization and parsing.

---
+++ src/renderer/curate/format/parser.ts
@@ -0,0 +1,183 @@
+import { CFTokenizer, tokenToString } from './tokenizer';
+
+/** An object representation of text that follow the Curation Format. */
+export type CurationFormatObject = {
+  [key: string]: string | string[] | CurationFormatObject;
+};
+
+/**
+ * Parse Curation Format tokens into a Curation Format object.
+ * @param tokens Tokens to parse into an object.
+ */
+export function parseCurationFormat(tokens: CFTokenizer.AnyToken[]): CurationFormatObject {
+  try {
+    return parseObject(tokens, {
+      startIndex: 0,
+      lastIndex: -1
+    });
+  } catch (error) {
+    console.error(tokens)
+    throw error;
+  }
+}
+
+/**
+ * Parse a single object from a set of tokens.
+ * The "state" parameter object contains some properties that are used as additional
+ * arguments for the parser, while some properties are used as additional return values.
+ * @param tokens Tokens to parse from (not all tokens must be parsed).
+ * @param state State of the parser (this is mutated during the parsing).
+ * @returns The parsed object.
+ */
+function parseObject(tokens: CFTokenizer.AnyToken[], state: ParseState): CurationFormatObject {
+  const parsed: CurationFormatObject = {};
+  // Error function
+  const createError = (text: string, token?: CFTokenizer.AnyToken): Error => {
+    return new Error(
+      'Failed to parse object.\n'+
+      text+'\n'+
+      'State:\n'+
+      `  Token Index: ${index}\n`+
+      `  Iterations: ${iterations} / ${maxIterations}\n`+
+      (token ? (
+        'Token:\n'+
+        tokenToString(token)
+      ) : '')
+    );
+  };
+  // Iteration state (used to abort in case it enters an infinite loop)
+  let iterations = 0;
+  const maxIterations = 1 + (tokens.length - state.startIndex);
+  // Parse the tokens, one by one
+  let index = state.startIndex;
+  mainLoop: // (Label used to make it possible to exit the loop while in a nested loop)
+  while (index < tokens.length) {
+    // Abort if it exceeds the maximum number of allowed iterations
+    iterations += 1;
+    if (iterations > maxIterations) {
+      throw createError(
+        'Maximum number of iterations was exceeded. \n'+
+        'This indicates that there is a bug in the parser.'
+      );
+    }
+    // Parse token
+    const token = tokens[index];
+    switch(token.type) {
+      // (Tokens to ignore)
+      case CFTokenizer.TokenType.COMMENT:
+        index += 1;
+        break;
+      // (Unexpected tokens)
+      case CFTokenizer.TokenType.VALUE:
+      case CFTokenizer.TokenType.LIST_ITEM:
+        throw createError('Unexpected token.', token);
+      // End object
+      case CFTokenizer.TokenType.INDENT_CHANGE:
+        if (token.delta < 0) {
+          break mainLoop; // (Exit the main loop)
+        } else {
+          throw createError(
+            'Indent Token was expected to be negative, but it was not.', 
+            token
+          );
+        }
+        break;
+      // Identifier token, create a property and assign the next value to it
+      case CFTokenizer.TokenType.IDENTIFIER: {
+        // Get the next non-comment token
+        const nextTokenIndex = indexOfNonComment(tokens, index + 1);
+        const nextToken = tokens[nextTokenIndex];
+        if (!nextToken) {
+          throw createError(
+            'Failed to assign value to identifier. Unexpected end of tokens.',
+            token
+          );
+        }
+        // Assign the contents of the token as the value
+        switch(nextToken.type) {
+          // Assign a string value
+          case CFTokenizer.TokenType.VALUE:
+            parsed[token.name] = nextToken.value;
+            index += 2;
+            break;
+          // Assign a list
+          case CFTokenizer.TokenType.LIST_ITEM: {
+            const list: string[] = [];
+            for (var i = nextTokenIndex; i < tokens.length; i++) {
+              const itemToken = tokens[i];
+              if (itemToken.type === CFTokenizer.TokenType.LIST_ITEM) {
+                list.push(itemToken.value);
+              } else { break; }
+            }
+            parsed[token.name] = list;
+            // Set the index to the token after the last list item
+            index = i;
+          } break;
+          // Assign an object (or end this object)
+          case CFTokenizer.TokenType.INDENT_CHANGE:
+            // Check if this is the start of a new object
+            if (nextToken.delta > 0) {
+              // Parse and assign the child object
+              const childState: ParseState = {
+                startIndex: nextTokenIndex + 1,
+                lastIndex: -1,
+              };
+              parsed[token.name] = parseObject(tokens, childState);
+              // Update the index to skip the tokens parsed by the child object
+              index = childState.lastIndex;
+            }
+            // Check if this is the end of the current object
+            else if (nextToken.delta < 0) {
+              // Assign an empty string
+              parsed[token.name] = '';
+              // End the current object
+              break mainLoop; // (Exit the main loop)
+            } else {
+              throw createError(
+                'Failed to assign value to identifier. Indent Token has a delta of 0.',
+                nextToken
+              );
+            }
+            break;
+          // Unexpected token type
+          default:
+            throw createError(
+              'Failed to assign value to identifier. Unexpected token type.',
+              token
+            );
+        }
+      } break;
+    }
+  }
+  // Update state
+  state.lastIndex = index;
+  // Done
+  return parsed;
+}
+
+/** State of the parser. */
+type ParseState = {
+  /**
+   * Index of the token to begin parsing at.
+   * This is an "additional argument" for the parser function.
+   */
+  startIndex: number;
+  /**
+   * Index of the token that was parsed last.
+   * This is an "additional return value" for the parser function.
+   */
+  lastIndex: number;
+};
+
+/**
+ * Get the index of the first non-comment token (from first to last).
+ * @param tokens Tokens to search through.
+ * @param startIndex First index to check.
+ * @returns The index of the first non-comment token, or -1 if none was found.
+ */
+function indexOfNonComment(tokens: CFTokenizer.AnyToken[], startIndex: number = 0): number {
+  for (let i = startIndex; i < tokens.length; i++) {
+    if (tokens[i].type !== CFTokenizer.TokenType.COMMENT) { return i; }
+  }
+  return -1;
+}
---
+++ src/renderer/curate/format/tokenizer.ts
@@ -0,0 +1,258 @@
+import { deepCopy } from '../../../shared/Util';
+
+export namespace CFTokenizer {
+  /** Type identifiers for all tokens. */
+  export enum TokenType {
+    COMMENT,
+    IDENTIFIER,
+    INDENT_CHANGE,
+    LIST_ITEM,
+    VALUE,
+  };
+
+  // ------ Token Sub-Types ------
+
+  /** Base type of all tokens. */
+  type TokenBase<T extends TokenType> = {
+    /** Token type identifier. */
+    type: T;
+  };
+
+  // ------ Token Types ------
+
+  /** A comment (they always take up and entire line of text). */
+  export type CommentToken = TokenBase<TokenType.COMMENT> & {
+    /** Text of the comment. */
+    content: string;
+  };
+
+  /** An identifier (the "name" part of a field/property assignment). */
+  export type IdentifierToken = TokenBase<TokenType.IDENTIFIER> & {
+    /** Name of the identifier. */
+    name: string;
+  };
+
+  /** A change in the indentation level. */
+  export type IndentChangeToken = TokenBase<TokenType.INDENT_CHANGE> & {
+    /** Change in the indentation level (positive is increase, negative is decrease, should never be 0). */
+    delta: number;
+  };
+
+  /** A list item. */
+  export type ListItemToken = TokenBase<TokenType.LIST_ITEM> & {
+    /** Value of the list item. */
+    value: string;
+  };
+
+  /** A value (a string value, single- or multi-line). */
+  export type ValueToken = TokenBase<TokenType.VALUE> & {
+    /** The value. */
+    value: string;
+  };
+
+  /** A combination of all token types. */
+  export type AnyToken = (
+    CommentToken |
+    IdentifierToken |
+    IndentChangeToken |
+    ListItemToken |
+    ValueToken
+  );
+}
+
+/** State of the tokenizer (that is carried over between lines of text). */
+type TokenizerState = {
+  /** If it is currently inside the value of a multi-line declaration. */
+  inMultiLine: boolean;
+  /** Chunks of strings of the current multi-line declaration (if any). */
+  multiLineChunks: string[];
+  /** The indent each row of text in the current multi-line declaration should have. */
+  multiLineIndent: number;
+  /** The index the multi-line token should be inserted at. */
+  multiLineIndex: number;
+  /** Current level of indentation. Used to measure when it changes. */
+  indent: number;
+};
+
+/**
+ * Parse a string following the Curation Format into tokens.
+ * These tokens can later be parsed into an object representation of the original text.
+ * @param text Text to parse into tokens.
+ */
+export function tokenizeCurationFormat(text: string): CFTokenizer.AnyToken[] {
+  const tokens: CFTokenizer.AnyToken[] = [];
+  const lines = text.replace(/\r/g, '').split('\n');
+  // State
+  const state: TokenizerState = {
+    inMultiLine: false,
+    multiLineChunks: [],
+    multiLineIndent: 0,
+    multiLineIndex: 0,
+    indent: 0,
+  };
+  // Tokenize the text, line by line
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    // Check if the line is a comment
+    if (line[0] === '#') {
+      // Add comment token
+      tokens.push({
+        type: CFTokenizer.TokenType.COMMENT,
+        content: line.substring(1),
+      });
+    } else {
+      // Count spaces and indents
+      const spaces = countSpacesLeft(line);
+      const indent = spacesToIndent(spaces);
+      // Check if the indentation level changed
+      if (indent !== state.indent) {
+        // Check if the previous token is an indent token
+        const prevToken = tokens[i - 1];
+        if (prevToken && prevToken.type === CFTokenizer.TokenType.INDENT_CHANGE) {
+          // Add current delta to the previous token
+          prevToken.delta += indent - state.indent;
+          // Remove token if delta is 0
+          if (prevToken.delta === 0) {
+            tokens.splice(i - 1, 1);
+          }
+        } else {
+          // Add indent token
+          tokens.push({
+            type: CFTokenizer.TokenType.INDENT_CHANGE,
+            delta: indent - state.indent
+          });
+        }
+      }
+      // Check if the multi-line value has ended (by a change in indentation)
+      if (state.inMultiLine && indent !== state.multiLineIndent) {
+        // Consume state and add a value value token
+        if (state.inMultiLine) {
+          applyMultiLine(state, tokens);
+        }
+      }
+      // Tokenize the remaining contents of the line
+      if (state.inMultiLine) {
+        // Copy data to state chunks
+        state.multiLineChunks.push(line.substring(indentToSpaces(indent)));
+      } else {
+        // Check if it is a list item
+        if (line[spaces] === '-' && line[spaces + 1] === ' ') {
+          // Add list item token
+          tokens.push({
+            type: CFTokenizer.TokenType.LIST_ITEM,
+            value: line.substr(spaces + 1).trimLeft(),
+          });
+        } else {
+          // Check if it is an identifier/value pair
+          const sepIndex = line.indexOf(':');
+          if (sepIndex >= 0) {
+            // Extract the name and value
+            const fieldName  = line.substring(spaces, sepIndex);
+            const fieldValue = line.substring(sepIndex + 1).trimLeft();
+            // Add identifier token
+            tokens.push({
+              type: CFTokenizer.TokenType.IDENTIFIER,
+              name: fieldName,
+            });
+            // Check if there is any value
+            if (fieldValue.length > 0) {
+              // Check if this is the start of a multi-line declaration
+              if (fieldValue === '|') {
+                // Prepare state for tokenizing the multi-line value
+                state.inMultiLine = true;
+                state.multiLineIndent = indent + 1;
+                state.multiLineIndex = tokens.length;
+              }
+              else {
+                // Add value token
+                tokens.push({
+                  type: CFTokenizer.TokenType.VALUE,
+                  value: fieldValue,
+                });
+              }
+            }
+          }
+        }        
+      }
+      // Update state
+      state.indent = indent;
+    }
+  }
+  // Apply remaining multi-line state (if any)
+  applyMultiLine(state, tokens);
+  // Done
+  return tokens;
+}
+
+/**
+ * Create a string representation of a token.
+ * This is meant to be used for error messages and debugging.
+ * @param token Token to create string of.
+ */
+export function tokenToString(token: CFTokenizer.AnyToken): string {
+  const displayToken: any = deepCopy(token);
+  displayToken.type = CFTokenizer.TokenType[token.type] || 'UNKNOWN';
+  return JSON.stringify(displayToken, undefined, 2)
+}
+
+/**
+ * Consume the current multi-line state, then create and add a token from it.
+ * If there is no multi-line state to consume, do nothing.
+ * @param state State of the tokenizer.
+ * @param tokens Array to add the token to.
+ */
+function applyMultiLine(state: TokenizerState, tokens: CFTokenizer.AnyToken[]): void {
+  // Check if the tokenizer is currently in a multi-line declaration
+  if (state.inMultiLine) {
+    // Insert value token
+    tokens.splice(state.multiLineIndex, 0, {
+      type: CFTokenizer.TokenType.VALUE,
+      value: state.multiLineChunks.join('\n'),
+    });
+    // Reset state
+    state.inMultiLine = false;
+    state.multiLineChunks.length = 0; // (This removes all items in the array)
+    state.multiLineIndent = 0;
+    state.multiLineIndex = 0;
+  }
+}
+
+/**
+ * Count the number of space characters on the left side of a string.
+ * @param str String to check.
+ */
+function countSpacesLeft(str: string): number {
+  let count = 0;
+  // (This is a "label", it here to make it possible to break out of the loop from
+  // inside the nested switch statement)
+  characterLoop:
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      // A space character
+      case ' ':
+        count += 1;
+        break;
+      // Not a space character
+      default: break characterLoop;
+    }
+  }
+  return count;
+}
+
+/**
+ * Calculate the indentation level for a number of spaces.
+ * @param spaces Number of spaces.
+ * @returns The indentation that number of spaces makes up.
+ */
+function spacesToIndent(spaces: number): number {
+  return (spaces * 0.25) | 0;
+}
+
+/**
+ * Calculate the number of spaces from an indentation level.
+ * @param indent Indentation level.
+ * @returns The number of spaces that makes up that indentation level.
+ */
+function indentToSpaces(indent: number): number {
+  return (indent * 4) | 0;
+}
