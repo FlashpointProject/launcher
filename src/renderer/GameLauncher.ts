bf403b021b18c6fd15806f77e6b8a20127d44dda
Colin Berry <colinb969@gmail.com>
2019-07-22 15:22:44 +0100

    Remove redundant quotations

--- src/renderer/GameLauncher.ts
+++ src/renderer/GameLauncher.ts
@@ -245,9 +245,9 @@ export class GameLauncher {
       ...((process.platform === 'linux') ? { http_proxy: 'http://localhost:22500/' } : null),
       // Add Wine related env vars if it's running through Wine
       ...(useWine ? {
-        "WINEPREFIX": path.join(window.External.config.fullFlashpointPath, 'FPSoftware/wineprefix'),
-        "WINEARCH": 'win32',
-        "WINEDEBUG": '-all',
+        WINEPREFIX: path.join(window.External.config.fullFlashpointPath, 'FPSoftware/wineprefix'),
+        WINEARCH: 'win32',
+        WINEDEBUG: '-all',
       } : null),
       // Copy this processes environment variables
       ...process.env,
