b8e5351b3b2bb7d3c417739c1dce48365025d9c1
bubba <darkclow3r@gmail.com>
2019-08-13 10:35:05 +0200

    style: change order of properties of converted meta properties
    This makes exported meta files easier to read.

--- src/renderer/curate/metaToMeta.ts
+++ src/renderer/curate/metaToMeta.ts
@@ -8,23 +8,23 @@ import { IGameInfo, IAdditionalApplicationInfo } from '../../shared/game/interfa
 export function convertToCurationMeta(game: IGameInfo, addApps?: IAdditionalApplicationInfo[]): CurationFormatMeta {
   const parsed: CurationFormatMeta = {};
   // Game meta
-  parsed['Application Path']     = game.applicationPath;
-  parsed['Developer']            = game.developer;
-  parsed['Extreme']              = game.extreme ? 'Yes' : 'No';
-  parsed['Game Notes']           = game.notes;
-  parsed['Languages']            = game.language;
-  parsed['Launch Command']       = game.launchCommand;
-  parsed['Original Description'] = game.originalDescription;
-  parsed['Play Mode']            = game.playMode;
-  parsed['Platform']             = game.platform;
-  parsed['Publisher']            = game.publisher;
-  parsed['Release Date']         = game.releaseDate;
-  parsed['Series']               = game.series;
-  parsed['Source']               = game.source;
-  parsed['Status']               = game.status;
-  parsed['Tags']                 = game.genre;
   parsed['Title']                = game.title;
+  parsed['Series']               = game.series;
+  parsed['Developer']            = game.developer;
+  parsed['Publisher']            = game.publisher;
+  parsed['Play Mode']            = game.playMode;
+  parsed['Release Date']         = game.releaseDate;
   parsed['Version']              = game.version;
+  parsed['Languages']            = game.language;
+  parsed['Extreme']              = game.extreme ? 'Yes' : 'No';
+  parsed['Tags']                 = game.genre;
+  parsed['Source']               = game.source;
+  parsed['Platform']             = game.platform;
+  parsed['Status']               = game.status;
+  parsed['Application Path']     = game.applicationPath;
+  parsed['Launch Command']       = game.launchCommand;
+  parsed['Game Notes']           = game.notes;
+  parsed['Original Description'] = game.originalDescription;
   // Add-apps meta
   const parsedAddApps: CurationFormatAddApps = {};
   if (addApps) {
@@ -51,8 +51,8 @@ export function convertToCurationMeta(game: IGameInfo, addApps?: IAdditionalAppl
         }
         // Add add-app
         parsedAddApps[heading] = {
-          'Application Path': addApp.applicationPath,
           'Heading': addApp.name,
+          'Application Path': addApp.applicationPath,
           'Launch Command': addApp.commandLine,
         };
       }
