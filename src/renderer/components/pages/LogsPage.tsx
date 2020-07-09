2b35af2ea26f467aab222b2218faf899a2d20c91
Jesper Gustafsson <darkclow3r@gmail.com>
2020-07-09 12:46:32 +0200

    fix: Update source labels array and order it alphabetically
    * Add "Launcher" to the source labels array
    * Remove "Redirector" from the source labels array
    * Order the array alphabetically

--- src/renderer/components/pages/LogsPage.tsx
+++ src/renderer/components/pages/LogsPage.tsx
@@ -20,12 +20,12 @@ export type LogsPageProps = OwnProps & WithPreferencesProps;
 const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/;
 const sourceLabels = [
   'Background Services',
+  'Curation',
   'Game Launcher',
   'Language',
-  'Redirector',
-  'Server',
-  'Curation',
+  'Launcher',
   'Log Watcher',
+  'Server',
 ];
 const levelLabels = [
   LogLevel[0],
