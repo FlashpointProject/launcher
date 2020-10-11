bdb5e4eec086b7636fc2b2db8ab57dd8df99c8d1
Jesper Gustafsson <darkclow3r@gmail.com>
2020-10-11 04:43:25 +0200

    feat: List all loaded curations in the curate page
    I imported useMouse from a side project since it was handy (although a
    bit of an overkill at the moment)

--- src/renderer/components/pages/CuratePage.tsx
+++ src/renderer/components/pages/CuratePage.tsx
@@ -1,16 +1,22 @@
 import { CurateBox } from '@renderer/components/CurateBox';
 import { WithCurateStateProps } from '@renderer/containers/withCurateState';
+import { WithMainStateProps } from '@renderer/containers/withMainState';
 import { WithPreferencesProps } from '@renderer/containers/withPreferences';
 import { WithTagCategoriesProps } from '@renderer/containers/withTagCategories';
-import { Curation } from '@renderer/store/curate/types';
-import * as React from 'react';
+import { useMouse } from '@renderer/hooks/useMouse';
 import { CurateActionType } from '@renderer/store/curate/enums';
+import { Curation } from '@renderer/store/curate/types';
+import { findElementAncestor, getPlatformIconURL } from '@renderer/Util';
+import { uuid } from '@renderer/util/uuid';
+import * as React from 'react';
+
+const index_attr = 'data-index';
 
 type OwnProps = {
 
 }
 
-export type CuratePageProps = OwnProps & WithPreferencesProps & WithTagCategoriesProps & WithCurateStateProps
+export type CuratePageProps = OwnProps & WithPreferencesProps & WithTagCategoriesProps & WithMainStateProps & WithCurateStateProps
 
 export function CuratePage(props: CuratePageProps) {
   const curation: Curation | undefined = props.curate.curations[props.curate.current];
@@ -23,38 +29,50 @@ export function CuratePage(props: CuratePageProps) {
     }
   }, []);
 
-  /*
-  const curation: EditCuration = {
-    key: '',
-    meta: {},
-    addApps: [],
-    content: [],
-    screenshot: {
-      data: undefined,
-      rawData: undefined,
-      exists: false,
-      fileName: undefined,
-      filePath: undefined,
-      version: 0,
+  const [onListMouseDown, onListMouseUp] = useMouse<number>(() => ({
+    chain_delay: 500,
+    find_id: (event) => {
+      let index: number | undefined;
+      try { index = findAncestorRowIndex(event.target as Element); }
+      catch (error) { console.error(error); }
+      return index;
     },
-    thumbnail: {
-      data: undefined,
-      rawData: undefined,
-      exists: false,
-      fileName: undefined,
-      filePath: undefined,
-      version: 0,
+    on_click: (event, id, clicks) => {
+      if (event.button === 0 && clicks === 1) { // Single left click
+        props.dispatchCurate({
+          type: CurateActionType.SET_CURRENT_CURATION,
+          index: id,
+        });
+      }
     },
-    locked: false,
-    delete: false,
-    deleted: false,
-  };
-  */
+  }));
+
+  const onNewCuration = React.useCallback(() => {
+    props.dispatchCurate({
+      type: CurateActionType.CREATE_CURATION,
+      folder: uuid(),
+    });
+  }, []);
 
   return (
     <div className='curate-page'>
-      <div className='curate-page__left'>
-        @TODO LIST CURATIONS HERE
+      <div
+        className='curate-page__left'
+        onMouseDown={onListMouseDown}
+        onMouseUp={onListMouseUp}>
+        {props.curate.curations.map((curation, index) => (
+          <div
+            className='curate-list-item'
+            key={curation.folder}
+            { ...{ [index_attr]: index } }>
+            <div
+              className='curate-list-item__icon'
+              style={{ backgroundImage: `url('${getPlatformIconURL('Flash'/* curation.meta.platform*/, props.main.logoVersion)}')` }} />
+            <p className='curate-list-item__title'>
+              {curation.folder}
+            </p>
+          </div>
+        ))}
       </div>
       <div className='curate-page__center simple-scroll'>
         { curation ? (
@@ -63,13 +81,26 @@ export function CuratePage(props: CuratePageProps) {
             dispatch={props.dispatchCurate} />
         ) : (
           <div>
-            No curation here.
+            No curation selected.
           </div>
         )}
       </div>
       <div className='curate-page__right'>
-        @TODO ADD BUTTONS AND STUFF HERE
+        <div onClick={onNewCuration}>New Curation</div>
       </div>
     </div>
   );
+}
+
+function findAncestorRowIndex(element: Element): number | undefined {
+  const ancestor = findElementAncestor(element, target => target.getAttribute(index_attr) !== null, true);
+  if (!ancestor) { return undefined; }
+
+  const index = ancestor.getAttribute(index_attr);
+  if (typeof index !== 'string') { throw new Error('Failed to get attribute from ancestor!'); }
+
+  const index_number = (index as any) * 1; // Coerce to number
+  if (isNaN(index_number)) { throw new Error('Failed to convert attribute to a number!'); }
+
+  return index_number;
 }
--- src/renderer/containers/ConnectedCuratePage.ts
+++ src/renderer/containers/ConnectedCuratePage.ts
@@ -1,9 +1,10 @@
 import { Subtract } from '@shared/interfaces';
 import { CuratePage, CuratePageProps } from '../components/pages/CuratePage';
 import { withCurateState, WithCurateStateProps } from './withCurateState';
+import { withMainState, WithMainStateProps } from './withMainState';
 import { withPreferences, WithPreferencesProps } from './withPreferences';
 import { withTagCategories, WithTagCategoriesProps } from './withTagCategories';
 
-export type ConnectedCuratePageProps = Subtract<CuratePageProps, WithPreferencesProps & WithTagCategoriesProps & WithCurateStateProps>;
+export type ConnectedCuratePageProps = Subtract<CuratePageProps, WithPreferencesProps & WithTagCategoriesProps & WithMainStateProps & WithCurateStateProps>;
 
-export const ConnectedCuratePage = withTagCategories(withPreferences(withCurateState(CuratePage)));
+export const ConnectedCuratePage = withTagCategories(withPreferences(withMainState(withCurateState(CuratePage))));
