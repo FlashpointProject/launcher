8b9d26a6118bcc7f6a84042c7ac9714e00dff42b
Colin Berry <colinb969@gmail.com>
2021-09-16 17:42:42 +0100

    refactor: Quote cleanup
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/shared/preferences/util.ts
+++ src/shared/preferences/util.ts
@@ -90,7 +90,7 @@ export const defaultPreferencesData: Readonly<AppPreferencesData> = Object.freez
   showBrokenGames: false,
   minimizedHomePageBoxes: [],
   hideExtremeScreenshots: true,
-  updateFeedUrl: "https://bluemaxima.org/flashpoint/updateFeed/stable.txt",
+  updateFeedUrl: 'https://bluemaxima.org/flashpoint/updateFeed/stable.txt',
 });
 
 /**
