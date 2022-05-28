47847b68c73903b5c2a3acb4b7c5f59e3c2c3757
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2022-05-28 08:01:57 -0400

    Make ConfigPage wait before displaying new logos.

--- src/shared/utils/throttle.ts
+++ src/shared/utils/throttle.ts
@@ -4,6 +4,10 @@ import { AnyFunction, ArgumentTypesOf } from '../interfaces';
 interface CallableCopy<T extends AnyFunction> extends Function {
   (...args: ArgumentTypesOf<T>): void;
 }
+/** A callable object that has the same argument types as T (and Promise<void> as the return type). */
+interface CallableCopyAsync<T extends AnyFunction> extends Function {
+  (...args: ArgumentTypesOf<T>): Promise<void>;
+}
 
 /**
  * Executes a callback immediately and starts a timer, only executes if timer is finished
@@ -43,6 +47,30 @@ export function delayedThrottle<T extends AnyFunction>(callback: T, time:number)
       timeout = undefined;
       callback(...args);
     }, time);
+  };
+  return throttler;
+}
+
+/**
+ * Executes a callback after a `time` millisecond timer, only starting the timer if it doesn't exist
+ * @param callback Called when the timer ends
+ * @param time Time in milliseconds before calling
+ */
+ export function delayedThrottleAsync<T extends AnyFunction>(callback: T, time:number): CallableCopyAsync<T> {
+  // Store timeout
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+  // Function that receives and records the events
+  const throttler: CallableCopyAsync<T> = async function(...args) {
+    return new Promise(resolve => {
+      // Check if currently throttling
+      if (timeout != undefined) { resolve(); }
+      // Release event after some time
+      timeout = setTimeout(async function() {
+        timeout = undefined;
+        await callback(...args);
+        resolve();
+      }, time);
+    });
   };
   return throttler;
 }
