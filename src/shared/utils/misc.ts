1c817df82b61c2528cc75370df1858e08b607047
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2022-06-30 12:06:47 -0400

    style: make the linter happy

--- src/shared/utils/misc.ts
+++ src/shared/utils/misc.ts
@@ -1,9 +1,9 @@
 export function chunkArray<T>(array: T[], chunkSize: number): T[][] {
-    const chunks: T[][] = [];
-  
-    for (let i = 0; i < array.length; i += chunkSize) {
-      chunks.push(array.slice(i, i + chunkSize));
-    }
-  
-    return chunks;
+  const chunks: T[][] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
   }
+
+  return chunks;
+}
