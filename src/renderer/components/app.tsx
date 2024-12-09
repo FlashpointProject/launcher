b7ace3ed67077dc8c7114b41889cb61a4150ffeb
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-12-09 22:45:34 +0100

    fix: Remove unnecessary rejection for FPFSS token consent when previous consent is false

--- src/renderer/components/app.tsx
+++ src/renderer/components/app.tsx
@@ -674,8 +674,6 @@ export class App extends React.Component<AppProps> {
           this.performFpfssAction(async (user) => {
             resolve(user);
           });
-        } else if (previousConsent === false) {
-          reject(new Error('Extension is not allowed to access FPFSS token'));
         } else {
           const msg = formatString(this.props.main.lang.dialog.extFpfssConsent, extId) as string;
           remote.dialog.showMessageBox({
